d6eb7a3f8179700c6f2ff446fc5f34597589278c
tfikadu <tfikadu76@gmail.com>
2023-07-26 18:44:59 -0700

    log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,50 +1,40 @@
 #!/usr/bin/python3
+"""0-stats module
 """
-log parsing
-"""
-
 import sys
-import re
 
 
-def output(log: dict) -> None:
+stats = {
+    '200': 0, '301': 0, '400': 0, '401': 0,
+    '403': 0, '404': 0, '405': 0, '500': 0
+}
+total = 0
+count = 0
+
+
+def print_stats(stats, total):
+    """print_stats function
     """
-    helper function to display stats
-    """
-    print("File size: {}".format(log["file_size"]))
-    for code in sorted(log["code_frequency"]):
-        if log["code_frequency"][code]:
-            print("{}: {}".format(code, log["code_frequency"][code]))
+    print("File size: {}".format(total))
+    for key, value in sorted(stats.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
 
 
 if __name__ == "__main__":
-    regex = re.compile(
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
-
-    line_count = 0
-    log = {}
-    log["file_size"] = 0
-    log["code_frequency"] = {
-        str(code): 0 for code in [
-            200, 301, 400, 401, 403, 404, 405, 500]}
-
     try:
         for line in sys.stdin:
-            line = line.strip()
-            match = regex.fullmatch(line)
-            if (match):
-                line_count += 1
-                code = match.group(1)
-                file_size = int(match.group(2))
-
-                # File size
-                log["file_size"] += file_size
-
-                # status code
-                if (code.isdecimal()):
-                    log["code_frequency"][code] += 1
-
-                if (line_count % 10 == 0):
-                    output(log)
+            data = line.split()
+            if len(data) > 4:
+                status = data[-2]
+                if status in stats.keys():
+                    stats[status] += 1
+                total += int(data[-1])
+                count += 1
+            if count == 10:
+                count = 0
+                print_stats(stats, total)
+    except Exception:
+        pass
     finally:
-        output(log)
+        print_stats(stats, total)
